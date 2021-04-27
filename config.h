e096232da9f0f4f42a40f4e1131068a0443cd524
adamglw <53101365+adamglw@users.noreply.github.com>
2021-04-27 23:54:29 +0100

    Minor edits

--- config.h
+++ config.h
@@ -112,25 +112,25 @@ float alpha = 0.85;
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#282828", /* hard contrast: #1d2021 | med contrast: #282828 | soft contrast: #32302f */
+  [0] = "#282828", /* bg      */
   [1] = "#cc241d", /* red     */
   [2] = "#98971a", /* green   */
   [3] = "#d79921", /* yellow  */
   [4] = "#458588", /* blue    */
-  [5] = "#b16286", /* magenta */
-  [6] = "#689d6a", /* cyan    */
-  [7] = "#a89984", /* white   */
+  [5] = "#b16286", /* purple  */
+  [6] = "#689d6a", /* aqua    */
+  [7] = "#a89984", /* gray    */
 
   /* 8 bright colors */
-  [8]  = "#928374", /* black   */
+  [8]  = "#928374", /* gray    */
   [9]  = "#fb4934", /* red     */
   [10] = "#b8bb26", /* green   */
   [11] = "#fabd2f", /* yellow  */
   [12] = "#83a598", /* blue    */
-  [13] = "#d3869b", /* magenta */
-  [14] = "#8ec07c", /* cyan    */
-  [15] = "#ebdbb2", /* white   */
-
+  [13] = "#d3869b", /* purple  */
+  [14] = "#8ec07c", /* aqua    */
+  [15] = "#ebdbb2", /* fg      */
+  
   [255] = 0,
 
   /*More colors can be added after 255 to use with DefaultXX */
