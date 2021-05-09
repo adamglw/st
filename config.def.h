da50b7ee423cb9f51290c04f395ad037c818e3a8
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-09 19:18:36 +0100

    reduce alpha

--- config.def.h
+++ config.def.h
@@ -103,10 +103,10 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.90;
+float alpha = 0.95;
  
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -134,17 +134,18 @@ static const char *colorname[] = {
   [255] = 0,
 
   /*More colors can be added after 255 to use with DefaultXX */
-  [256] = "#191919", /* Kesseki */
+  [256] = "#262626", /* Iwa */
+  [257] = "#cacaca", /* foreground */
+  [258] = "#e7e7e7", /* Cursor color*/
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 15;
+unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-/*unsigned int defaultbg = 258;*/
-static unsigned int defaultcs = 15;
+static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
